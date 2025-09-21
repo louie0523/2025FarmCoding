ebab5dd3370e62c00602fabbdcd9b941022b2457
louie0523 <151519353+louie0523@users.noreply.github.com>
2025-09-22 07:27:52 +0900

    Update TImeManager.cs

--- TImeManager.cs
+++ TImeManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class TImeManager : MonoBehaviour
 {
@@ -8,10 +9,7 @@ public class TImeManager : MonoBehaviour
 
     public float currentTime;
     public float timeSpeed = 5f;
-    public Material Sky;
-    public float SkyMatMax = 5.2f;
-    public float SkyMatMin = 1.8f;
-    public Light DefaultLight;
+    public PostProcessVolume voluem;
     public Color MaxLightColor;
     public Color MinLightColor;
     
@@ -40,13 +38,13 @@ public class TImeManager : MonoBehaviour
 
     void TimeSet()
     {
-        Sky = RenderSettings.skybox;
+        ColorGrading colorGrading;
+        voluem.profile.TryGetSettings<ColorGrading>(out colorGrading);
 
-        float t = Mathf.Sin(Mathf.PI * currentTime / 24f);
+        float t = Mathf.Clamp01((Mathf.Sin((currentTime - 6f) * Mathf.PI / 12f) + 1f) * 0.5f);
 
-        float ex = Mathf.Lerp(SkyMatMin, SkyMatMin, t);
-        Sky.SetFloat("_Exposure", ex);
+        Color lightColor = Color.Lerp(MinLightColor, MaxLightColor, t);
 
-        DefaultLight.color = Color.Lerp(MinLightColor, MaxLightColor, t);
+        colorGrading.colorFilter.Override(lightColor);
     }
 }
